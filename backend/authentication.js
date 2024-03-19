c7022f81d32d8d48a28417eacc34ee0ed4ab04f1
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-03-19 15:25:51 -0700

    docs: update code comments

--- backend/authentication.js
+++ backend/authentication.js
@@ -2,14 +2,15 @@ const express = require('express')
 const bcrypt = require('bcrypt')
 const app = express()
 
-// CONSTANTS //
 
  const CREATED_USER_201          = 201
  const BAD_REQUEST_400           = 400
  const CONFLICT_409              = 409
  const INTERNAL_SERVER_ERROR_500 = 500
 
- const SALT_ROUNDS = 10 // User for password hashing
+ const SALT_ROUNDS   = 10 // User for password hashing
+ const MAX_API_CALLS = 20 // Max number of API calls
+
 
 /* An Express method to configure middleware; has access to requests
 and response objects, plus the next middleware function to be run. */
@@ -29,25 +30,24 @@ app.get('/users', (req, res) => {
 app.post('/users', async (req, res) => {
     try {
 
-        // Error is user doesn't provide name, email, or password
         if (req.body.name == null || req.body.email == null || req.body.password == null) {
             return res.status(BAD_REQUEST_400).send("Missing name, email, or password")
         }
 
-        // Error if user already exists
         if (users.find(user => user.email === req.body.email)) {
             return res.status(CONFLICT_409).send("User already exists")
         }
 
         /* Hashes the password; takes the original plain-text password and a
         salt, which is the complexity of the hashing process -- a higher number
-        means more time, more complexity, and more security.  */
+        means more time, more complexity, and more security. */
         const hashedPassword = await bcrypt.hash(req.body.password, SALT_ROUNDS)
 
         const user = {
             name: req.body.name,
             email: req.body.email,
-            password: hashedPassword
+            password: hashedPassword,
+            api_calls: 0
         }
 
         // User successfully created and stored
@@ -76,6 +76,7 @@ app.post("/users/login", async (req, res) => {
     }
 
     try {
+        // User found; entered password same as stored password
         if (await bcrypt.compare(req.body.password, user.password)) {
             res.send('Success')
         } else {
