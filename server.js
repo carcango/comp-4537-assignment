49aea1400395e7d026290ffca4efe9530beefe14
Carson.O <carcango@gmail.com>
2024-03-20 15:50:08 -0700

    Added chat

--- server.js
+++ server.js
@@ -4,137 +4,217 @@
 // TODO: Add specific route for API calls; add middleware to track API calls
 // TODO: Add user-facing messages to separate file
 
-const User = require('./user')
-const { RESPONSE_CODES, RESPONSE_MSG, MAX_TOKEN_AGE_IN_MS, MAX_API_CALLS } = require('./constants')
+const User = require("./user");
+const {
+  RESPONSE_CODES,
+  RESPONSE_MSG,
+  MAX_TOKEN_AGE_IN_MS,
+  MAX_API_CALLS,
+} = require("./constants");
+const cors = require("cors");
+const fetch = require("node-fetch");
+const dotenv = require("dotenv");
 
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
-const SECRET_KEY = process.env.SECRET_KEY
+dotenv.config({ path: ".env.local" });
 
-require('dotenv').config() // Load environment variable: secret key for JWT
+const express = require("express");
+const app = express();
+app.use(cors());
+app.use(express.json());
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const SECRET_KEY = process.env.SECRET_KEY;
+
+require("dotenv").config(); // Load environment variable: secret key for JWT
 
 // Allows Express to parse JSON and cookie data for middleware
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
 // Array to store users; used for testing, will be replaced with database
-const users = []
+const users = [];
 
 /// ////////////////////
 // Get Current Users ///
 /// ////////////////////
-app.get('/users', (_, res) => {
-  res.json(users)
-})
+app.get("/users", (_, res) => {
+  res.json(users);
+});
 
 /// /////////////////////////////////////////
 // Create User, Hash Password, Store User ///
 /// /////////////////////////////////////////
-app.post('/users', async (req, res) => {
+app.post("/users", async (req, res) => {
   try {
     // Check payload for email and password; ensure they exist
     if (req.body.email == null || req.body.password == null) {
-      return res.status(RESPONSE_CODES.BAD_REQUEST_400).send(RESPONSE_MSG.MISSING_INFO_400)
+      return res
+        .status(RESPONSE_CODES.BAD_REQUEST_400)
+        .send(RESPONSE_MSG.MISSING_INFO_400);
     }
 
     // Will need to update this to use database search
-    if (users.find(user => user.email === req.body.email)) {
-      return res.status(RESPONSE_CODES.CONFLICT_409).send(RESPONSE_MSG.ALREADY_EXISTS_409)
+    if (users.find((user) => user.email === req.body.email)) {
+      return res
+        .status(RESPONSE_CODES.CONFLICT_409)
+        .send(RESPONSE_MSG.ALREADY_EXISTS_409);
     }
 
-    const user = await User.create(req.body.email, req.body.password)
+    const user = await User.create(req.body.email, req.body.password);
 
-    users.push(user)
-    res.status(RESPONSE_CODES.CREATED_USER_201).send(RESPONSE_MSG.SUCCESSFULLY_REGISTERED_201)
+    users.push(user);
+    res
+      .status(RESPONSE_CODES.CREATED_USER_201)
+      .send(RESPONSE_MSG.SUCCESSFULLY_REGISTERED_201);
   } catch (error) {
-    console.log(error)
-    res.status(RESPONSE_CODES.SERVER_ERROR_500).send(RESPONSE_MSG.SERVER_ERROR_500)
+    console.log(error);
+    res
+      .status(RESPONSE_CODES.SERVER_ERROR_500)
+      .send(RESPONSE_MSG.SERVER_ERROR_500);
   }
-})
+});
 
 /// /////////////
 // User Login ///
 /// /////////////
-app.post('/users/login', async (req, res) => {
+app.post("/users/login", async (req, res) => {
   if (req.body.email == null || req.body.password == null) {
-    return res.status(RESPONSE_CODES.BAD_REQUEST_400).send(RESPONSE_MSG.MISSING_INFO_400)
+    return res
+      .status(RESPONSE_CODES.BAD_REQUEST_400)
+      .send(RESPONSE_MSG.MISSING_INFO_400);
   }
 
-  const user = users.find(user => user.email === req.body.email)
+  const user = users.find((user) => user.email === req.body.email);
   if (user == null) {
-    return res.status(RESPONSE_CODES.NOT_FOUND_404).send(RESPONSE_MSG.NOT_FOUND_404)
+    return res
+      .status(RESPONSE_CODES.NOT_FOUND_404)
+      .send(RESPONSE_MSG.NOT_FOUND_404);
   }
 
   try {
     if (await bcrypt.compare(req.body.password, user.password)) {
       // Create token; user email is the payload (used to identify user later on)
-      const token = jwt.sign({ userEmail: user.email }, SECRET_KEY, { expiresIn: MAX_TOKEN_AGE_IN_MS })
+      const token = jwt.sign({ userEmail: user.email }, SECRET_KEY, {
+        expiresIn: MAX_TOKEN_AGE_IN_MS,
+      });
 
       /* Set token in HTTP-only cookie
             > httpOnly: true - cookie cannot be accessed by client-side scripts
             > secure: true - cookie will only be sent over HTTPS; set to false for testing */
-      res.cookie('token', token, {
-        httpOnly: true,
-        secure: false,
-        maxAge: MAX_TOKEN_AGE_IN_MS
-      })
-        .send(RESPONSE_MSG.OK_200)
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          maxAge: MAX_TOKEN_AGE_IN_MS,
+        })
+        .send(RESPONSE_MSG.OK_200);
     } else {
-      res.send(RESPONSE_MSG.UNAUTHORIZED_401)
+      res.send(RESPONSE_MSG.UNAUTHORIZED_401);
     }
   } catch {
-    res.status(RESPONSE_CODES.SERVER_ERROR_500).send(RESPONSE_MSG.SERVER_ERROR_500)
+    res
+      .status(RESPONSE_CODES.SERVER_ERROR_500)
+      .send(RESPONSE_MSG.SERVER_ERROR_500);
   }
-})
+});
+
+/// ///////////////////////
+// Handle Chat Messages ///
+/// ///////////////////////
+const API_URL = "https://api.anthropic.com";
+const API_TOKEN = process.env.ANTHROPIC_API_TOKEN;
+const ANTHROPIC_VERSION = "2023-06-01";
+
+app.post("/chat", async (req, res) => {
+  const { message } = req.body;
+
+  try {
+    const response = await fetch(`${API_URL}/v1/messages`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-Key": API_TOKEN,
+        "anthropic-version": ANTHROPIC_VERSION,
+      },
+      body: JSON.stringify({
+        model: "claude-3-haiku-20240307",
+        system: "Roleplay to simulate a text-based adventure game",
+        messages: [
+          {
+            role: "user",
+            content: message,
+          },
+        ],
+        max_tokens: 200,
+      }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    if (response.ok) {
+      const assistantReply = data.content[0].text;
+      res.json({ message: assistantReply });
+    } else {
+      console.error("Error:", data);
+      res.status(500).json({ error: "An error occurred" });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
 
 /// ///////////////
 // User API URL ///
 /// ///////////////
 
-app.listen(3000, () => console.log('Server started; listening on Port 3000'))
+app.listen(3000, () => console.log("Server started; listening on Port 3000"));
 
 /// /////////////
 // Middleware ///
 /// /////////////
-function authenticateToken (req, res, next) {
-  const token = req.cookies.token
+function authenticateToken(req, res, next) {
+  const token = req.cookies.token;
 
-  if (token == null) return res.sendStatus(RESPONSE_CODES.UNAUTHORIZED_401)
+  if (token == null) return res.sendStatus(RESPONSE_CODES.UNAUTHORIZED_401);
 
   jwt.verify(token, SECRET_KEY, (err, decoded) => {
-    if (err) return res.sendStatus(RESPONSE_CODES.FORBIDDEN_403)
+    if (err) return res.sendStatus(RESPONSE_CODES.FORBIDDEN_403);
 
     // Use email from decoded token to find user
     // Update to check database for user
-    const user = users.find(user => user.email === decoded.userEmail)
-    if (!user) return res.sendStatus(RESPONSE_CODES.UNAUTHORIZED_401)
+    const user = users.find((user) => user.email === decoded.userEmail);
+    if (!user) return res.sendStatus(RESPONSE_CODES.UNAUTHORIZED_401);
 
-    req.user = user
+    req.user = user;
 
-    next()
-  })
+    next();
+  });
 }
 
-function trackApiCalls (req, res, next) {
+function trackApiCalls(req, res, next) {
   // Get user email from decoded token; find user based on email
   // Update to check database for user
-  const userEmail = req.user.email
-  const user = users.find(user => user.email === userEmail)
+  const userEmail = req.user.email;
+  const user = users.find((user) => user.email === userEmail);
 
   if (!user) {
-    return res.status(RESPONSE_CODES.UNAUTHORIZED_401).send(RESPONSE_MSG.UNAUTHORIZED_401)
+    return res
+      .status(RESPONSE_CODES.UNAUTHORIZED_401)
+      .send(RESPONSE_MSG.UNAUTHORIZED_401);
   }
 
-  user.api_call_counter++
+  user.api_call_counter++;
 
   if (user.api_call_counter > MAX_API_CALLS) {
-    return res.status(RESPONSE_CODES.FORBIDDEN_403).send(RESPONSE_MSG.API_LIMIT_EXCEEDED_403)
+    return res
+      .status(RESPONSE_CODES.FORBIDDEN_403)
+      .send(RESPONSE_MSG.API_LIMIT_EXCEEDED_403);
   }
 
   // Update counter in database for persistence (to be implemented)
 
-  next()
+  next();
 }
