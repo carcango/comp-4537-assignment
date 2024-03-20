0e5d42330080d57cc008ad8a14aeb8119e6956af
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-03-20 09:24:27 -0700

    refactor: create User class; add email validator

---
+++ user.js
@@ -0,0 +1,33 @@
+// TODO: Check if user email already registered
+
+import { isEmail } from 'validator'
+import { hash } from 'bcrypt'
+
+const SALT_ROUNDS = 10
+
+class User {
+    constructor(email, password) {
+        this.email = email
+        this.password = password
+        this.api_counter = 0
+    }
+
+    static async create(email, password) {
+        if (email == null || password == null) {
+            throw new Error("Email and password are required")
+        }
+        if (!User.isValidEmail(email)) {
+            throw new Error("Email is invalid")
+        }
+        const hashedPassword = User.hashPassword(password)
+        return new User(email, hashedPassword)
+    }
+
+    static async hashPassword(password) {
+        return await hash(password, SALT_ROUNDS)
+    }
+
+    static isValidEmail(email) {
+        return isEmail(email)
+    }
+}
