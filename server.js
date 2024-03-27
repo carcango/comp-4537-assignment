8e3c10700bf270f6416bfc28b3879293da61b442
Dakaro Mueller <LukarioMC@gmail.com>
2024-03-27 14:20:54 -0700

    Hopefully fixed CORS #2

--- server.js
+++ server.js
@@ -21,11 +21,19 @@ dotenv.config({ path: '.env.local' })
 
 const express = require('express')
 const app = express()
-app.use(
-  cors({
-    credentials: true
-  })
-) // Enable CORS for all routes
+const allowedOrigins = ['http://localhost:3000/', 'https://comp-4537-frontend.vercel.app/']
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+  },
+  credentials: true // reflect the request's credentials mode
+})) // Enable CORS for all routes
 app.options('*', cors())
 app.use(express.json())
 const bcrypt = require('bcrypt')
