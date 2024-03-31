a247ee9e42a8f0fd31450256256519821245715d
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-03-31 13:30:50 -0700

    refactor apiTracking

---
+++ apiMiddleware.js
@@ -0,0 +1,27 @@
+const User = require('./user')
+const {
+  RESPONSE_CODES,
+  RESPONSE_MSG,
+  MAX_API_CALLS
+} = require('./constants')
+
+const trackApiCalls = async (req, res, next) => {
+  const userEmail = req.user.email
+
+  const user = await User.findOne({ where: { email: userEmail } })
+  if (!user) {
+    return res
+      .status(RESPONSE_CODES.UNAUTHORIZED_401)
+      .send(RESPONSE_MSG.UNAUTHORIZED_401)
+  }
+  user.apiCallCounter++
+  await user.save()
+  if (user.apiCallCounter > MAX_API_CALLS) {
+    return res
+      .status(RESPONSE_CODES.FORBIDDEN_403)
+      .send(RESPONSE_MSG.API_LIMIT_EXCEEDED_403)
+  }
+  next()
+}
+
+module.exports = { trackApiCalls }
--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 // TODO: Create separate user search function (update for database search)
-// TODO: Refactor using OOP principles
-// TODO: Write API tracking function; update for database
 // TODO: Add specific route for API calls; add middleware to track API calls
 // TODO: Add user-facing messages to separate file
 
 const User = require('./user')
 const {
   RESPONSE_CODES,
-  RESPONSE_MSG,
-  MAX_API_CALLS
+  RESPONSE_MSG
 } = require('./constants')
+
 const cors = require('cors')
-const fetch = require('node-fetch')
 const dotenv = require('dotenv')
-const OpenAI = require('openai')
 const sequelize = require('./db')
 
 dotenv.config({ path: '.env.local' })
@@ -22,6 +18,8 @@ const express = require('express')
 const app = express()
 
 const authenticateToken = require('./authMiddleware')
+const trackApiCalls = require('./apiMiddleware')
+
 const { userRegistration, userLogin } = require('./authController')
 const { handleChatMessages } = require('./chatController')
 const { handleImageGeneration } = require('./imageController')
@@ -31,19 +29,11 @@ app.use(cors({
   credentials: true // reflect the request's credentials mode
 })) // Enable CORS for all routes
 app.options('*', cors())
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
-const SECRET_KEY = process.env.SECRET_KEY
 
 // Allows Express to parse JSON and cookie data for middleware
 app.use(express.json())
 app.use(cookieParser())
-
-const OPENAI_API_KEY = process.env.OPENAI_API_TOKEN
-const openai = new OpenAI({
-  apiKey: OPENAI_API_KEY
-})
 
 // Sync the database models
 sequelize.sync()
@@ -73,40 +63,4 @@ app.post('/chat', authenticateToken, trackApiCalls, handleChatMessages)
 // Handle image generation
 app.post('/generate-image', authenticateToken, trackApiCalls, handleImageGeneration)
 
-/// ///////////////
-// User API URL ///
-/// ///////////////
 app.listen(process.env.PORT, () => console.log(`Server started; listening on Port ${process.env.PORT}`))
-
-/// /////////////
-// Middleware ///
-/// /////////////
-
-async function trackApiCalls (req, res, next) {
-  // Get user email from decoded token; find user based on email
-  const userEmail = req.user.email
-
-  const user = await User.findOne({
-    where: {
-      email: userEmail
-    }
-  })
-  if (!user) {
-    return res
-      .status(RESPONSE_CODES.UNAUTHORIZED_401)
-      .send(RESPONSE_MSG.UNAUTHORIZED_401)
-  }
-  user.apiCallCounter++
-  await user.save()
-  if (user.apiCallCounter > MAX_API_CALLS) {
-    return res
-      .status(RESPONSE_CODES.FORBIDDEN_403)
-      .send(RESPONSE_MSG.API_LIMIT_EXCEEDED_403)
-  }
-  next()
-}
-
-function logError (route, error) {
-  const timeString = Date.now().toString()
-  console.error(`[${timeString}][${route}] ${error}`)
-}
