aaee5604aa7fecc32a26accb22ba7e977ff66aa3
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-03-31 12:05:05 -0700

    Add comment to user.js

--- user.js
+++ user.js
@@ -1,44 +1,52 @@
-// user.js
-const Sequelize = require("sequelize");
-const sequelize = require("./db");
-const bcrypt = require("bcrypt");
-const INITIAL_API_COUNTER = 0;
-const SALT_ROUNDS = 10;
-const User = sequelize.define("User", {
+const Sequelize = require('sequelize')
+const sequelize = require('./db')
+const bcrypt = require('bcrypt')
+
+// Constants for user model
+const API_START_COUNT = 0
+const PASSWORD_SALTING = 10
+
+const User = sequelize.define('User', {
   email: {
     type: Sequelize.STRING,
     allowNull: false,
     unique: true,
     validate: {
-      isEmail: true,
-    },
+      isEmail: true
+    }
   },
   password: {
     type: Sequelize.STRING,
-    allowNull: false,
+    allowNull: false
   },
   apiCallCounter: {
     type: Sequelize.INTEGER,
-    defaultValue: INITIAL_API_COUNTER,
+    defaultValue: API_START_COUNT
   },
   isAdmin: {
     type: Sequelize.BOOLEAN,
-    defaultValue: false,
-  },
-});
+    defaultValue: false
+  }
+})
+
 // Creating a user is asynchronous because hashing can be slow
 User.createUser = async function (userData) {
-  const { email, password } = userData;
-  const existingUser = await User.findOne({ where: { email } });
+  const { email, password } = userData
 
+  // Check if the user already exists
+  const existingUser = await User.findOne({ where: { email } })
   if (existingUser) {
-    throw new Error("Email is already registered");
+    throw new Error('Email is already registered')
   }
-  if (!password) {
-    throw new Error("Password is required");
-  }
-  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-  return await User.create({ email, password: hashedPassword });
-};
 
-module.exports = User;
+  // Ensure password is provided
+  if (!password) {
+    throw new Error('Password is required')
+  }
+
+  // Hash password before saving it to the databse
+  const hashedPassword = await bcrypt.hash(password, PASSWORD_SALTING)
+  return await User.create({ email, password: hashedPassword })
+}
+
+module.exports = User
