8121ea92bd5033b344b69d78076bb9f06d7325ee
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-03-31 12:58:09 -0700

    Refactored userLogin

--- authController.js
+++ authController.js
@@ -43,102 +43,51 @@ exports.userRegistration = async (req, res) => {
       .send(RESPONSE_MSG.SERVER_ERROR_500)
   }
 }
-//
 
-app.post('/users', async (req, res) => {
+exports.userLogin = async (req, res) => {
   try {
-    // Check payload for email, password; ensure they exist
-    if (req.body.email == null || req.body.password == null) {
-      return res
-        .status(RESPONSE_CODES.BAD_REQUEST_400)
-        .send(RESPONSE_MSG.MISSING_INFO_400)
-    }
-    // Check if user already exists
-    if (
-      await User.findOne({
-        where: {
-          email: req.body.email
-        }
-      })
-    ) {
-      return res
-        .status(RESPONSE_CODES.CONFLICT_409)
-        .send(RESPONSE_MSG.ALREADY_EXISTS_409)
-    }
-
-    const user = await User.createUser({
-      email: req.body.email,
-      password: req.body.password
+    // Check that user is registered
+    const existingUser = await User.findOne({
+      where: { email: req.body.email }
     })
 
-    // Create token; user email is the payload (used to identify user later on)
-    const token = jwt.sign({ userEmail: user.email }, SECRET_KEY, {
-      expiresIn: MAX_TOKEN_AGE_IN_MS
-    })
-
-    res
-      .status(RESPONSE_CODES.CREATED_USER_201)
-      .json({ token, message: RESPONSE_MSG.SUCCESSFULLY_REGISTERED_201 })
-  } catch (error) {
-    logError('users', 'Error creating user! ' + error)
-    res
-      .status(RESPONSE_CODES.SERVER_ERROR_500)
-      .send(RESPONSE_MSG.SERVER_ERROR_500)
-  }
-})
-
-/// /////////////
-// User Login ///
-/// /////////////
-app.post('/users/login', async (req, res) => {
-  try {
-    if (req.body.email == null || req.body.password == null) {
-      return res
-        .status(RESPONSE_CODES.BAD_REQUEST_400)
-        .send(RESPONSE_MSG.MISSING_INFO_400)
-    }
-
-    const user = await User.findOne({
-      where: {
-        email: req.body.email
-      }
-    })
-
-    if (user == null) {
+    // If user is not found, return an error
+    if (!existingUser) {
       return res
         .status(RESPONSE_CODES.NOT_FOUND_404)
         .send(RESPONSE_MSG.NOT_FOUND_404)
     }
 
-    const isPasswordValid = await bcrypt.compare(
-      req.body.password,
-      user.password
-    )
-
-    if (!isPasswordValid) {
+    /* If user is found, check that password is valid by
+    comparing the provided password with the hashed password */
+    const isValidPassword = await bcrypt.compare(req.body.password, existingUser.password)
+    if (!isValidPassword) {
       return res
         .status(RESPONSE_CODES.UNAUTHORIZED_401)
         .send(RESPONSE_MSG.UNAUTHORIZED_401)
     }
 
-    // Create token; user email is the payload (used to identify user later on)
-    const token = jwt.sign({ userEmail: user.email }, SECRET_KEY, { expiresIn: MAX_TOKEN_AGE_IN_MS })
-    res
-      .cookie('token', token, {
-        httpOnly: true,
-        secure: true,
+    // If password is valid, create a token and send it to user via cookie
+    const token = jwt.sign(
+      { userEmail: existingUser.email },
+      SECRET_KEY,
+      { expiresIn: MAX_TOKEN_AGE_IN_MS })
+
+    res.cookie('token', token,
+      {
+        httpOnly: false, // Set to true in production
+        secure: false, // Set to true in production
         maxAge: MAX_TOKEN_AGE_IN_MS
       })
+
+    // Notify user that login was successful
+    res
       .status(RESPONSE_CODES.OK_200)
-      .json({ message: RESPONSE_MSG.OK_200 })
+      .send(RESPONSE_MSG.OK_200)
   } catch (error) {
-    logError('users/login', 'Error on user login! ' + error)
+    console.error('Error logging in user! ' + error)
     res
       .status(RESPONSE_CODES.SERVER_ERROR_500)
       .send(RESPONSE_MSG.SERVER_ERROR_500)
   }
-})
-
-app.get('/verify-token', authenticateToken, (req, res) => {
-  res.status(200).send({ message: 'Token is valid' })
-})
+}
